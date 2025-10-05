37d6515bac260eb66934441a412d7b4a63024ffa
sunsetsavorer <sunset.savorer@gmail.com>
2025-10-05 20:09:29 +0300

    feat: add app layer

---
+++ app/app.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"github.com/sunsetsavorer/tcp-chat.git/config"
+	"github.com/sunsetsavorer/tcp-chat.git/server"
+)
+
+type App struct {
+	Config *config.Config
+	Server *server.ChatServer
+}
+
+func NewApp() *App {
+
+	return &App{}
+}
+
+func (a *App) Run() error {
+
+	config := config.New()
+
+	err := config.LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	a.Config = config
+	a.Server = server.New(a.Config)
+
+	err = a.Server.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
